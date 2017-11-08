c2e63196a0a7adac0cc4be763206f478cbc3ab39
antoniolatela <alatel01@dcs.bbk.ac.uk>
2017-11-08 16:02:05 +0000

    first commit

--- src/uk/bbk/popii/firslesson/HelloWorld.java
+++ src/uk/bbk/popii/firslesson/HelloWorld.java
@@ -3,16 +3,16 @@ package uk.bbk.popii.firslesson;
 public class HelloWorld extends FirstClass{
     public static void main(String a[]){
         HelloWorld hw = new HelloWorld();
-        //hw.inherit();
+        hw.inherit();
         hw.printa(1);
 
     }
 
-    public void inherit(){
+    private void inherit(){
         printa();
     }
 
-    public void printa(int i){
+    protected void printa(int i){
         System.out.println("Hello World!");
     }
 }
