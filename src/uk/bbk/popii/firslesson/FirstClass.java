059d9abed4e404724f1e21ff1573d9f2fe78e8ba
antoniolatela <alatel01@dcs.bbk.ac.uk>
2017-11-08 15:57:26 +0000

    first commit

--- src/uk/bbk/popii/firslesson/FirstClass.java
+++ src/uk/bbk/popii/firslesson/FirstClass.java
@@ -2,14 +2,21 @@ package uk.bbk.popii.firslesson;
 
 public class FirstClass {
     public void printa () {
-        for (int i = 0; i<100; i++) {
+        for (int i = 0; i<5; i++) {
             System.out.println(i+" Hi Ant");
         }
+        if (true) System.out.println("Hi Ant Final");
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i++ +" Hi Ant");
+        }
+        i = 0;
+        do {
+            System.out.println(i++ + " Hi Ant");
+        } while (i < 5);
     }
-    public static void main(String a[]){
-        FirstClass fc = new FirstClass();
-        fc.printa();
-    }
+
 }
 
 
